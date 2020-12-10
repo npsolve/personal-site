c22551c657a6ec2238bbfc9ce25a02710a966c7b
Rahul Ranjan <rahul.rrixe@gmail.com>
2020-12-10 02:50:03 +0100

    Remove manifest.json link

--- src/components/Favicon.js
+++ src/components/Favicon.js
@@ -5,7 +5,6 @@ export default () => {
   return (
     <Helmet>
       <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#ffffff" />
     </Helmet>
   );
